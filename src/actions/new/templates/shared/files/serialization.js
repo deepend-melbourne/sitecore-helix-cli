3dbbf42f2e9d917a46bf1c735106a7d1e314c7e0
Brendan McKenzie <hello@brendanmckenzie.com>
2018-08-24 14:44:26 +1000

    Removed Renderings from configuration

--- src/actions/new/templates/shared/files/serialization.js
+++ src/actions/new/templates/shared/files/serialization.js
@@ -5,7 +5,6 @@ module.exports = (name, layer) => `<configuration xmlns:patch="http://www.siteco
         <configuration name="${layer}.${name}" description="${layer} ${name}" extends="Helix.${layer}">
           <predicate>
             <include name="Media Library" database="master" path="/sitecore/media library/${layer}/${name}" />
-            <include name="Renderings" database="master" path="/sitecore/layout/Renderings/${layer}/${name}" />
             <include name="Placeholder Settings" database="master" path="/sitecore/layout/Placeholder Settings/${layer}/${name}" />
             <include name="System Settings" database="master" path="/sitecore/system/Settings/${layer}/${name}" />
           </predicate>
